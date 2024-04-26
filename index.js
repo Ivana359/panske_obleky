022ebfbf959356c4431d83b71dc41283a6ee8c0d
Ivana Nováková <rempa@seznam.cz>
2024-04-26 11:22:52 +0200

    doplněné tlačítko pro červenou barvu, upraven js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('funguju');
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const kosile = document.getElementById("product-image");
+    const tlačítka = document.querySelectorAll('.color-button');
+  
+    tlačítka.forEach(function(tlačítko) {
+      tlačítko.addEventListener("click", function() {
+        const color = this.getAttribute('data-color');  
+        kosile.style.fill = color;  
+      });
+    });
+  });
